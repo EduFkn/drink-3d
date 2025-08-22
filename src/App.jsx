5dcdb069e7f91062094a53f09e8d59ba51ed93fb
EduFkn <ploutzin129@gmail.com>
2025-08-22 03:47:42 -0300

    chore: initial setup project on drink bar lading page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const Page = () => {
+  return ( 
+
+    <div className="flex-center h-[100vh]">
+      oi
+    </div>
+   );
+}
+ 
+export default Page;
